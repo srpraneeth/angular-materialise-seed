f9604f05db32d35d47d2862e3bcb54c576f796ee
Praneeth Ramesh <sr.praneeth@gmail.com>
2017-05-08 22:24:38 -0400

    Seed Project with Router
    Seed Project with Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SecondComponent } from './components/second/second.component';
+import { FirstComponent } from './components/first/first.component';
+
+const appRoutes: Routes = [
+  { path: 'ulink1', component: FirstComponent },
+  { path: 'ulink2', component: SecondComponent },
+  {
+    path: 'ulink3',
+    component: FirstComponent,
+    data: { title: 'Heroes List' }
+  },
+  { path: '',
+    redirectTo: '/ulink1',
+    pathMatch: 'full'
+  },
+  { path: '**', component: SecondComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SecondComponent,
+    FirstComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
